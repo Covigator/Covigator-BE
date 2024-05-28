04b534178ad416ce6ae81e804ffceb9a253d9f3e
jaeuk520 <jaeuk520@naver.com>
2024-05-28 23:49:56 +0900

    feat: 초기 개발 환경 세팅

---
+++ src/test/java/com/covigator/Covigator/CovigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covigator.Covigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
