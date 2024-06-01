8b33384c11c0c8d8874d1326783d038c20ea35fe
jaeuk520 <jaeuk520@naver.com>
2024-06-01 23:05:16 +0900

    chore: CovigatorApplicationTests에 classes 속성 삽입

--- src/test/java/com/covigator/Covigator/CovigatorApplicationTests.java
+++ src/test/java/com/covigator/Covigator/CovigatorApplicationTests.java
@@ -3,7 +3,7 @@ package com.covigator.Covigator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CovigatorApplicationTests.class)
 class CovigatorApplicationTests {
 
 	@Test
