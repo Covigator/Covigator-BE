6065cb5c8a14a59d1d0f81f8542f270bb40d88f0
jaeuk520 <jaeuk520@naver.com>
2024-06-04 00:38:20 +0900

    [#11] feat: 이메일이 같아도 플랫폼이 다르면 중복 가입될 수 있도록 구현

--- src/main/java/com/ku/covigator/repository/MemberRepository.java
+++ src/main/java/com/ku/covigator/repository/MemberRepository.java
@@ -1,10 +1,13 @@
 package com.ku.covigator.repository;
 
 import com.ku.covigator.domain.member.Member;
+import com.ku.covigator.domain.member.Platform;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+    Optional<Member> findByEmailAndPlatform(String email, Platform platform);
 }
+
