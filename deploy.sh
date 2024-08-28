9f45755c3f1a9bd76e05f9e767bdc9673728d19c
jaeuk520 <jaeuk520@naver.com>
2024-08-28 21:40:32 +0900

    [#20] chore: deploy.sh 파일 수정

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,4 @@ else
 fi
 
 echo "> $JAR_PATH 배포" #3
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar $JAR_PATH >> $REPOSITORY/log/deploy.log 2 > $REPOSITORY/log/deploy_err.log &
