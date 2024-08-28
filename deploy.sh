d9729f9ad288c1f980503a6c9753c6dfb5e4e40b
jaeuk520 <jaeuk520@naver.com>
2024-08-28 21:26:17 +0900

    [#20] chore: deploy.sh 파일 수정

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,4 @@ else
 fi
 
 echo "> $JAR_PATH 배포" #3
-nohup java -jar \
-        -Dspring.profiles.active=dev \
-        build/libs/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
