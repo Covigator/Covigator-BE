63e5b5a0d96ea211161cd3432212becf0e512b38
jaeuk520 <jaeuk520@naver.com>
2024-09-04 23:22:10 +0900

    [#28] feat: 에러 메시지 slack에 전송 기능 구현

--- src/main/java/com/ku/covigator/controller/ControllerAdvice.java
+++ src/main/java/com/ku/covigator/controller/ControllerAdvice.java
@@ -2,12 +2,18 @@ package com.ku.covigator.controller;
 
 import com.ku.covigator.dto.response.ErrorResponse;
 import com.ku.covigator.exception.CovigatorException;
+import com.ku.covigator.support.SlackAlarmGenerator;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class ControllerAdvice {
+
+    private final SlackAlarmGenerator slackAlarmGenerator;
 
     @ExceptionHandler(CovigatorException.class)
     public ResponseEntity<ErrorResponse> handleCovigatorException(CovigatorException e) {
@@ -15,7 +21,8 @@ public class ControllerAdvice {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleInternalException() {
+    public ResponseEntity<ErrorResponse> handleInternalException(Exception e, HttpServletRequest request) {
+        slackAlarmGenerator.sendSlackAlertErrorLog(e, request);
         return ResponseEntity.internalServerError()
                 .body(new ErrorResponse(9999, "서버 복구중입니다. 잠시만 기다려주세요."));
     }
