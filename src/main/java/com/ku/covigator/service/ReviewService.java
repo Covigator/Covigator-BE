a4ad2c56689b7aef71426a7351dd8a4bedade606
jaeuk520 <jaeuk520@naver.com>
2024-09-04 15:23:18 +0900

    [#22] feat: 리뷰 조회 기능 구현

--- src/main/java/com/ku/covigator/service/ReviewService.java
+++ src/main/java/com/ku/covigator/service/ReviewService.java
@@ -4,13 +4,19 @@ import com.ku.covigator.domain.Course;
 import com.ku.covigator.domain.Review;
 import com.ku.covigator.domain.member.Member;
 import com.ku.covigator.dto.request.PostReviewRequest;
+import com.ku.covigator.dto.response.GetReviewResponse;
 import com.ku.covigator.exception.notfound.NotFoundCourseException;
 import com.ku.covigator.exception.notfound.NotFoundMemberException;
 import com.ku.covigator.repository.CourseRepository;
 import com.ku.covigator.repository.MemberRepository;
 import com.ku.covigator.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -31,5 +37,10 @@ public class ReviewService {
         Review review = request.toEntity(member, course);
 
         reviewRepository.save(review);
+    }
+
+    public GetReviewResponse findReviews(Pageable pageable, Long courseId) {
+        Slice<Review> reviews = reviewRepository.findByCourseId(pageable, courseId);
+        return GetReviewResponse.fromEntity(reviews);
     }
 }
--- src/test/java/com/ku/covigator/controller/ReviewControllerTest.java
+++ src/test/java/com/ku/covigator/controller/ReviewControllerTest.java
@@ -2,6 +2,7 @@ package com.ku.covigator.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ku.covigator.dto.request.PostReviewRequest;
+import com.ku.covigator.dto.response.GetReviewResponse;
 import com.ku.covigator.security.jwt.JwtAuthArgumentResolver;
 import com.ku.covigator.security.jwt.JwtAuthInterceptor;
 import com.ku.covigator.service.ReviewService;
@@ -10,11 +11,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.List;
+
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = ReviewController.class)
@@ -47,6 +55,47 @@ class ReviewControllerTest {
                         .content(objectMapper.writeValueAsString(request))
                 ).andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @DisplayName("리뷰 조회를 요청한다.")
+    @Test
+    void test() throws Exception {
+        //given
+        Long courseId = 1L;
+        GetReviewResponse.ReviewDto review = GetReviewResponse.ReviewDto.builder()
+                .author("김코비")
+                .comment("좋아요~~~")
+                .score(5)
+                .build();
+
+        GetReviewResponse.ReviewDto review2 = GetReviewResponse.ReviewDto.builder()
+                .author("박코비")
+                .comment("싫어요")
+                .score(1)
+                .build();
+
+        GetReviewResponse response = new GetReviewResponse(List.of(review, review2), false);
+
+        this.mockMvc = MockMvcBuilders.standaloneSetup(
+                        new ReviewController(this.reviewService)
+                )
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .build();
+        given(reviewService.findReviews(any(), any())).willReturn(response);
+
+        //when //then
+        mockMvc.perform(get("/community/courses/{course_id}/reviews", courseId))
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$.has_next").value(false),
+                        jsonPath("$.reviews[0].author").value("김코비"),
+                        jsonPath("$.reviews[0].comment").value("좋아요~~~"),
+                        jsonPath("$.reviews[0].score").value(5),
+                        jsonPath("$.reviews[1].author").value("박코비"),
+                        jsonPath("$.reviews[1].comment").value("싫어요"),
+                        jsonPath("$.reviews[1].score").value(1)
+                );
     }
 
 }
