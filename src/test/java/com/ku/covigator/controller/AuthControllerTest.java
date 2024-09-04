b81a25e4f66b6fea9eb366f14f05f3f5eedb6055
jaeuk520 <jaeuk520@naver.com>
2024-09-04 23:36:23 +0900

    [#28] chore: 컨트롤러 테스트에서 support 패키지를 컴포넌트 스캔의 대상이 되도록 수정

--- src/test/java/com/ku/covigator/controller/AuthControllerTest.java
+++ src/test/java/com/ku/covigator/controller/AuthControllerTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ComponentScan("com.ku.covigator.support")
 @WebMvcTest(controllers = AuthController.class)
 class AuthControllerTest {
 
--- src/test/java/com/ku/covigator/controller/CourseControllerTest.java
+++ src/test/java/com/ku/covigator/controller/CourseControllerTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ComponentScan("com.ku.covigator.support")
 @WebMvcTest(controllers = CourseController.class)
 class CourseControllerTest {
 
