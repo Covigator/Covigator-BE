23dab5cc1b6fdc1ef65983a33bcc65880a65f36d
jaeuk520 <jaeuk520@naver.com>
2024-09-17 13:29:49 +0900

    fix: preflight 500 에러 해결

--- src/main/java/com/ku/covigator/security/jwt/JwtAuthInterceptor.java
+++ src/main/java/com/ku/covigator/security/jwt/JwtAuthInterceptor.java
@@ -5,6 +5,7 @@ import com.ku.covigator.exception.jwt.JwtUnsupportedTokenException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -18,10 +19,19 @@ public class JwtAuthInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
+        if (validatePreflight(request)) {
+            return true;
+        }
+
         String requestHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
         String accessToken = jwtProvider.getTokenFromRequestHeader(requestHeader);
         return jwtProvider.validateToken(accessToken);
 
+    }
+
+    public boolean validatePreflight(HttpServletRequest request) {
+
+        return StringUtils.equals(request.getMethod(), "OPTIONS");
     }
 
 }
