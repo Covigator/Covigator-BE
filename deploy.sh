1662e24a2dd17a97778d135a1313075519fa8bd1
jaeuk520 <jaeuk520@naver.com>
2024-09-22 22:13:37 +0900

    chore: 배포 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ] #2
+if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
@@ -16,5 +16,5 @@ else
   sleep 5
 fi
 
-echo "> $JAR_PATH 배포" #3
-nohup java -jar -Dspring.profiles.active=prod $JAR_PATH >> $REPOSITORY/log/deploy.log 2 > $REPOSITORY/log/deploy_err.log &
+echo "> $JAR_PATH 배포"
+nohup java -jar -Dspring.profiles.active=prod $JAR_PATH >> $REPOSITORY/log/deploy.log 2>> $REPOSITORY/log/deploy_err.log &
