900a55009b73c7007f076fc80798ec6868d059cd
jaeuk520 <jaeuk520@naver.com>
2024-09-27 17:50:48 +0900

    fix: 에러 코드 수정

--- src/main/java/com/ku/covigator/exception/badrequest/PasswordMismatchException.java
+++ src/main/java/com/ku/covigator/exception/badrequest/PasswordMismatchException.java
@@ -2,7 +2,7 @@ package com.ku.covigator.exception.badrequest;
 
 public class PasswordMismatchException extends BadRequestException{
     public PasswordMismatchException() {
-        super(1001, "비밀번호가 일치하지 않습니다.");
+        super(3001, "비밀번호가 일치하지 않습니다.");
     }
 
 }
