9f040edf77c92fee17bbdf05639b9c3fc492ce11
jaeuk520 <jaeuk520@naver.com>
2024-09-27 17:50:48 +0900

    fix: 에러 코드 수정

--- src/main/java/com/ku/covigator/exception/badrequest/DuplicateMemberException.java
+++ src/main/java/com/ku/covigator/exception/badrequest/DuplicateMemberException.java
@@ -6,6 +6,6 @@ import lombok.Getter;
 public class DuplicateMemberException extends BadRequestException{
 
     public DuplicateMemberException() {
-        super(1002, "이미 가입된 사용자입니다.");
+        super(3002, "이미 가입된 사용자입니다.");
     }
 }
