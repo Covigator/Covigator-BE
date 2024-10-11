a0b106d76bd6337913771672d292ae4afaed8b99
jaeuk520 <jaeuk520@naver.com>
2024-10-11 14:47:52 +0900

    [#64] feat: 일기 예보 API 호출 클래스를 WeatherForecastService로부터 분리

---
+++ src/main/java/com/ku/covigator/weather/WeatherProvider.java
@@ -0,0 +1,31 @@
+package com.ku.covigator.weather;
+
+import com.ku.covigator.dto.response.ShortTermWeatherForecastResponse;
+import com.ku.covigator.exception.weatherforecast.WeatherAPI4xxError;
+import com.ku.covigator.exception.weatherforecast.WeatherAPI5xxError;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.net.URI;
+
+@Component
+@RequiredArgsConstructor
+public class WeatherProvider {
+
+    private final RestClient weatherRestClient;
+
+    public ShortTermWeatherForecastResponse requestWeatherForecast(URI uri) {
+        return weatherRestClient.get()
+                .uri(uri)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+                    throw new WeatherAPI4xxError();
+                })
+                .onStatus(HttpStatusCode::is5xxServerError, (request, response) -> {
+                    throw new WeatherAPI5xxError();
+                })
+                .body(ShortTermWeatherForecastResponse.class);
+    }
+}
