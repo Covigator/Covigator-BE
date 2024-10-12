d1c65697e8dd333d043e1a034e4d3b5ed512e150
jaeuk520 <jaeuk520@naver.com>
2024-10-12 15:50:16 +0900

    [#64] refactor: 기준 시각 맵핑 시 NavigableMap을 사용하도록 구현

--- src/main/java/com/ku/covigator/weather/BaseTimeMapper.java
+++ src/main/java/com/ku/covigator/weather/BaseTimeMapper.java
@@ -1,41 +1,44 @@
 package com.ku.covigator.weather;
 
 import com.ku.covigator.exception.weatherforecast.BaseTimeMappingException;
+import lombok.experimental.UtilityClass;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
+@UtilityClass
 public class BaseTimeMapper {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("HHmm");
+
+    private static final NavigableMap<LocalTime, LocalTime> BASE_TIME_MAP = new TreeMap<>();
+
+    static {
+        BASE_TIME_MAP.put(LocalTime.of(23, 0), LocalTime.of(23, 0));
+        BASE_TIME_MAP.put(LocalTime.of(2, 0), LocalTime.of(2, 0));
+        BASE_TIME_MAP.put(LocalTime.of(5, 0), LocalTime.of(5, 0));
+        BASE_TIME_MAP.put(LocalTime.of(8, 0), LocalTime.of(8, 0));
+        BASE_TIME_MAP.put(LocalTime.of(11, 0), LocalTime.of(11, 0));
+        BASE_TIME_MAP.put(LocalTime.of(14, 0), LocalTime.of(14, 0));
+        BASE_TIME_MAP.put(LocalTime.of(17, 0), LocalTime.of(17, 0));
+        BASE_TIME_MAP.put(LocalTime.of(20, 0), LocalTime.of(20, 0));
+    }
 
     // 단기 예보 발표 시각에 맞게 기준 시각 변환
     public static String mapToBaseTime(LocalTime currentTime) {
 
-        LocalTime baseTime = null;
-
-        if (currentTime.isAfter(LocalTime.of(22, 59)) || currentTime.isBefore(LocalTime.of(2, 0))) {
-            baseTime = LocalTime.of(23, 0);
-        } else if (currentTime.isAfter(LocalTime.of(1, 59)) && currentTime.isBefore(LocalTime.of(5, 0))) {
-            baseTime = LocalTime.of(2, 0);
-        } else if (currentTime.isAfter(LocalTime.of(4, 59)) && currentTime.isBefore(LocalTime.of(8, 0))) {
-            baseTime = LocalTime.of(5, 0);
-        } else if (currentTime.isAfter(LocalTime.of(7, 59)) && currentTime.isBefore(LocalTime.of(11, 0))) {
-            baseTime = LocalTime.of(8, 0);
-        } else if (currentTime.isAfter(LocalTime.of(10, 59)) && currentTime.isBefore(LocalTime.of(14, 0))) {
-            baseTime = LocalTime.of(11, 0);
-        } else if (currentTime.isAfter(LocalTime.of(13, 59)) && currentTime.isBefore(LocalTime.of(17, 0))) {
-            baseTime = LocalTime.of(14, 0);
-        } else if (currentTime.isAfter(LocalTime.of(16, 59)) && currentTime.isBefore(LocalTime.of(20, 0))) {
-            baseTime = LocalTime.of(17, 0);
-        } else if (currentTime.isAfter(LocalTime.of(19, 59)) && currentTime.isBefore(LocalTime.of(23, 0))) {
-            baseTime = LocalTime.of(20, 0);
+        if (currentTime.isBefore(LocalTime.of(2, 0))) {
+            return LocalTime.of(23, 0).format(FORMATTER);
         }
+
+        LocalTime baseTime = BASE_TIME_MAP.floorEntry(currentTime).getValue();
 
         if (baseTime == null) {
             throw new BaseTimeMappingException();
         }
 
-        return baseTime.format(DateTimeFormatter.ofPattern("HHmm"));
-
+        return baseTime.format(FORMATTER);
     }
-
 }
