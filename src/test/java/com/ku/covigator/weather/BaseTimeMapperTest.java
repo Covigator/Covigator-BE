ab5b50a52f49173ce005c1307d8c9caa04853e32
jaeuk520 <jaeuk520@naver.com>
2024-10-12 16:41:20 +0900

    [#64] test: 불필요한 Import 제거

--- src/test/java/com/ku/covigator/weather/BaseTimeMapperTest.java
+++ src/test/java/com/ku/covigator/weather/BaseTimeMapperTest.java
@@ -1,10 +1,8 @@
 package com.ku.covigator.weather;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
