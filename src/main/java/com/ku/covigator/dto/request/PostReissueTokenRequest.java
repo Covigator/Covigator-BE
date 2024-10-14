487e0c2cff684fbe65819c3c7f702088b9af9cef
Jae Wook Park <jaeuk520@naver.com>
2024-10-14 14:43:20 +0900

    fix: PostReissueTokenRequest dto 필드명이 스네이크 케이스를 사용하도록 수정

--- src/main/java/com/ku/covigator/dto/request/PostReissueTokenRequest.java
+++ src/main/java/com/ku/covigator/dto/request/PostReissueTokenRequest.java
@@ -1,8 +1,10 @@
 package com.ku.covigator.dto.request;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.NotNull;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record PostReissueTokenRequest(
-
         @NotNull(message = "NULL일 수 없습니다.") String refreshToken) {
 }
