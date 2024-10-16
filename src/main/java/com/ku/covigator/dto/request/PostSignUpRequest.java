bdea7f5b2842367ff2176a93c9a75a4765430db4
jaeuk520 <jaeuk520@naver.com>
2024-10-16 20:54:22 +0900

    feat: 멤버 엔티티에 전화번호 추가

--- src/main/java/com/ku/covigator/dto/request/PostSignUpRequest.java
+++ src/main/java/com/ku/covigator/dto/request/PostSignUpRequest.java
@@ -12,6 +12,8 @@ import lombok.Builder;
 public record PostSignUpRequest(
         @NotBlank(message = "공백일 수 없습니다.")
         @Size(min = 1, max = 10, message = "글자 길이는 1~10자여야 합니다.") String nickname,
+        @Pattern(regexp = "^01\\d{9}$",
+                message = "올바른 휴대폰 번호 형식이 아닙니다.") String phoneNumber,
         @Email(message = "올바른 이메일 형식이 아닙니다.") String email,
         @Pattern(regexp = "^(?=.*[A-Za-z가-힣])(?=.*\\d)(?=.*[!@#$%^&*()_+~\\-=\\[\\]{};':\",./<>?\\\\|`]).{7,15}$",
                 message = "한글/영문, 숫자, 특수문자를 포함하여 7~15자를 입력해주세요.") String password) {
@@ -21,6 +23,7 @@ public record PostSignUpRequest(
                 .nickname(nickname)
                 .password(password)
                 .platform(Platform.LOCAL)
+                .phoneNumber(phoneNumber)
                 .build();
     }
 }
