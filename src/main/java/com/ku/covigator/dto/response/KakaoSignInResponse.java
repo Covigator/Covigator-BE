8da18559c41bc7f031c99906a1e861adbed9cbc0
jaeuk520 <jaeuk520@naver.com>
2024-10-27 16:58:59 +0900

    fix: 로컬/카카오 로그인 API response에 이미지 Url 추가

--- src/main/java/com/ku/covigator/dto/response/KakaoSignInResponse.java
+++ src/main/java/com/ku/covigator/dto/response/KakaoSignInResponse.java
@@ -4,14 +4,14 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public record KakaoSignInResponse(String accessToken, String isNew, String refreshToken, String nickname, String email) {
+public record KakaoSignInResponse(String accessToken, String isNew, String refreshToken, String nickname, String email, String imageUrl) {
 
-    public static KakaoSignInResponse fromNewMember(String accessToken, String refreshToken, String nickname, String email) {
-        return new KakaoSignInResponse(accessToken, "True", refreshToken, nickname, email);
+    public static KakaoSignInResponse fromNewMember(String accessToken, String refreshToken, String nickname, String email, String imageUrl) {
+        return new KakaoSignInResponse(accessToken, "True", refreshToken, nickname, email, imageUrl);
     }
 
-    public static KakaoSignInResponse fromOldMember(String accessToken, String refreshToken, String nickname, String email) {
-        return new KakaoSignInResponse(accessToken, "False", refreshToken, nickname, email);
+    public static KakaoSignInResponse fromOldMember(String accessToken, String refreshToken, String nickname, String email, String imageUrl) {
+        return new KakaoSignInResponse(accessToken, "False", refreshToken, nickname, email, imageUrl);
     }
 
 }
