a234f22b7517296ee52c2e49d19954797688e15b
jaeuk520 <jaeuk520@naver.com>
2024-10-29 18:16:51 +0900

    feat: 멤버 엔티티에 generation 필드 추가 & 여행 스타일 저장 시 입력받도록 구현

---
+++ src/main/java/com/ku/covigator/domain/member/Generation.java
@@ -0,0 +1,10 @@
+package com.ku.covigator.domain.member;
+
+import lombok.Getter;
+
+@Getter
+public enum Generation {
+    YOUNG_ADULT, // 10대 ~ 20대
+    MIDDLE_AGED, // 30대 ~ 40대
+    SENIOR // 50대 이상
+}
