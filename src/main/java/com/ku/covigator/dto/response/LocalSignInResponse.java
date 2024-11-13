5ed769187e1b2020ab87b3ee820bec2a87000e62
jaeuk520 <jaeuk520@naver.com>
2024-11-13 16:44:19 +0900

    fix: 로컬 로그인 API 응답 형식에 성별, 나이대 추가

--- src/main/java/com/ku/covigator/dto/response/LocalSignInResponse.java
+++ src/main/java/com/ku/covigator/dto/response/LocalSignInResponse.java
@@ -2,14 +2,18 @@ package com.ku.covigator.dto.response;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.ku.covigator.domain.member.Gender;
+import com.ku.covigator.domain.member.Generation;
 import com.ku.covigator.domain.travelstyle.*;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public record LocalSignInResponse(String accessToken, String refreshToken, String nickname, String email, String imageUrl, TravelStyleDto travelStyle) {
+public record LocalSignInResponse(String accessToken, String refreshToken, String nickname, String email, String imageUrl,
+                                  Gender gender, Generation generation, TravelStyleDto travelStyle) {
 
     public static LocalSignInResponse from(final String accessToken, final String refreshToken, final String nickname,
-                                    final String email, final String imageUrl, final TravelStyleDto travelStyleDto) {
-        return new LocalSignInResponse(accessToken, refreshToken, nickname, email, imageUrl, travelStyleDto);
+                                           final String email, final String imageUrl, final Gender gender,
+                                           final Generation generation, final TravelStyleDto travelStyleDto) {
+        return new LocalSignInResponse(accessToken, refreshToken, nickname, email, imageUrl, gender, generation, travelStyleDto);
     }
 
     @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
